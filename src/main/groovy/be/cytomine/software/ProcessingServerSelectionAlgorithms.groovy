c538a1b347ca1ab334c9808b02d884861632ec6a
13eMualliug <g.vissers13@gmail.com>
2019-04-10 14:05:34 +0200

    New class which will contains algorithms. Algorithms for choosing the best ProcessingServer Currently, there's only an Algo. This algo doesn't work yet... He will just return the id of the "good" processing server

---
+++ src/main/groovy/be/cytomine/software/ProcessingServerSelectionAlgorithms.groovy
@@ -0,0 +1,13 @@
+package be.cytomine.software
+
+import be.cytomine.client.models.ProcessingServer
+import org.json.simple.JSONObject
+
+class ProcessingServerSelectionAlgorithms {
+
+    static basicAlgorithm(Map< ProcessingServer,JSONObject> mapOfProcessingServers)
+    {
+        //return the goodPS
+        return new Long(44607)
+    }
+}
